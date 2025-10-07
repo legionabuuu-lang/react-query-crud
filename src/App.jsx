3317d5f83a4da8772a8241a26f94c302b12ecfd9
legionabuuu-lang <legion.abuuu@gmail.com>
2025-10-07 14:51:31 +0300

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Discounts from "./components/Discounts";
 import VideoSection from "./components/VideoSection";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Discounts />
       <VideoSection />
+      <Home />
     </div>
   );
 }
